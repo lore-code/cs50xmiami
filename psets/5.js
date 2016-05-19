f64b4105ba628bee0904ac731d8ae03eb082c855
Lorena Tomasini <lore.tomasini@gmail.com>
2016-05-19 10:04:23 -0400

    Couldn't figure out how to use prompt even when changing to html...

--- psets/5.js
+++ psets/5.js
@@ -1,7 +1,4 @@
-/*
-DrEvil!
-
-Create a function called DrEvil.
+/*Create a function called DrEvil.
 It should take a single argument, an amount, and return '<amount> dollars',
 except it will add '(pinky)' at the end if the amount is 1 million.
 
@@ -10,6 +7,18 @@ For example:
   DrEvil(1000000): 1000000 dollars (pinky)
 */
 
-function drEvil(amount) {
-  // write your solution here...
-}
+    function drEvil(amount) {
+
+
+        if (amount < 1000000)
+        var result = (amount + " dollars");
+        else
+        result = (amount + " pinky");
+        return result
+      }
+
+      console.log(drEvil(1000000));
+
+
+
+      // write your solutio n here...
